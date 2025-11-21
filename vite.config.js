f05081652c893328ada90fe64108ae028c20e386
Px211 <mars0911209070@gmail.com>
2025-11-21 13:20:31 +0800

    feat: re-build with github pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 【關鍵修改點】：這是告訴 Vite 應用程式部署到子路徑時要怎麼做
+  // 必須與您的 GitHub 倉庫名稱一樣
+  base: '/vue-todo-app/' 
 })
